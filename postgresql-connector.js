a9fa40afee838dbf9d9c034b4ce6219d5a837d53
enyil <enyil@github.com>
2023-05-19 14:24:19 -0500

    updating database

--- postgresql-connector.js
+++ postgresql-connector.js
@@ -305,8 +305,11 @@ class PostgreSQLConnector {
             if(!stepsTableExists) {
                 await client.query(stepsTableQuery);
                 console.log('steps table created');
+            };
+            const needMigration = await needMigration(client); // check if the tables need migration
+            if (needMigration) {
+                await migrateTables(client); // migrate the tables
             }
-            ;
         } catch (err) {
             ;
             console.error('Error creating tables:', err);
@@ -314,6 +317,113 @@ class PostgreSQLConnector {
         }
         return true;
     }
+
+    async needMigration(client) {
+      const checkTableQueries = [
+          `
+          SELECT data_type 
+          FROM information_schema.columns 
+          WHERE table_name = 'runs' 
+          AND column_name = 'id';
+          `,
+          `
+          SELECT data_type 
+          FROM information_schema.columns 
+          WHERE table_name = 'jobs' 
+          AND column_name = 'id';
+          `
+      ];
+  
+      try {
+          const results = await Promise.all(checkTableQueries.map(query => client.query(query)));
+  
+          // If the data_type of 'id' column in either 'runs' or 'jobs' table is 'bigint', then migration is needed
+          return results.some(result => result.rows[0].data_type === 'bigint');
+      } catch (err) {
+          console.error('Error checking tables:', err);
+          return false;
+      }
+  }
+
+  async migrateTables(client) {
+    // Begin transaction
+    await client.query('BEGIN');
+
+    try {
+        // Create temporary tables with new schema
+        const createTempTablesQueries = [
+            `
+            CREATE TABLE runs_temp AS TABLE runs WITH NO DATA;
+            ALTER TABLE runs_temp DROP COLUMN id;
+            ALTER TABLE runs_temp ADD COLUMN id SERIAL PRIMARY KEY;
+            ALTER TABLE runs_temp ADD COLUMN run_id BIGINT;
+            `,
+            `
+            CREATE TABLE jobs_temp AS TABLE jobs WITH NO DATA;
+            ALTER TABLE jobs_temp DROP COLUMN id;
+            ALTER TABLE jobs_temp ADD COLUMN id SERIAL PRIMARY KEY;
+            ALTER TABLE jobs_temp ADD COLUMN job_id BIGINT;
+            `,
+            `
+            CREATE TABLE steps_temp AS TABLE steps WITH NO DATA;
+            `
+        ];
+
+        for (const query of createTempTablesQueries) {
+            await client.query(query);
+        }
+
+        // Copy data from the old tables to the new ones with new mapping
+        await client.query(`
+            INSERT INTO runs_temp (run_id, name /* rest of fields */) 
+            SELECT id, name /* rest of fields */ 
+            FROM runs;
+        `);
+
+        await client.query(`
+            INSERT INTO jobs_temp (job_id, run_id, jobName /* rest of fields */) 
+            SELECT jobs.id, runs_temp.id, jobs.jobName /* rest of fields */ 
+            FROM jobs 
+            JOIN runs_temp ON runs_temp.run_id = jobs.run_id;
+        `);
+
+        await client.query(`
+            INSERT INTO steps_temp (job_id, stepName /* rest of fields */) 
+            SELECT jobs_temp.id, steps.stepName /* rest of fields */ 
+            FROM steps 
+            JOIN jobs_temp ON jobs_temp.job_id = steps.job_id;
+        `);
+
+        // Drop the old tables
+        const dropOldTablesQueries = [
+            `DROP TABLE runs;`,
+            `DROP TABLE jobs;`,
+            `DROP TABLE steps;`
+        ];
+
+        for (const query of dropOldTablesQueries) {
+            await client.query(query);
+        }
+
+        // Rename the new tables to the original names
+        const renameTablesQueries = [
+            `ALTER TABLE runs_temp RENAME TO runs;`,
+            `ALTER TABLE jobs_temp RENAME TO jobs;`,
+            `ALTER TABLE steps_temp RENAME TO steps;`
+        ];
+
+        for (const query of renameTablesQueries) {
+            await client.query(query);
+        }
+
+        // Commit transaction
+        await client.query('COMMIT');
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    }
+}
+
 }
 
 module.exports = PostgreSQLConnector;
