b494a45dec67585eabeddbafbb12f4e78318254e
David Wiggs <david-wiggs@github.com>
2023-05-22 15:21:25 -0500

    some debugging

--- terraform/main.tf
+++ terraform/main.tf
@@ -450,6 +450,7 @@ resource "azurerm_stream_analytics_job_schedule" "beaver" {
 
 data "github_actions_secrets" "beaver_secrets" {
   name      = "beaver"
+  owner = "octodemo"
 }
 
 resource "azurerm_linux_web_app" "beaver-app" {
@@ -462,7 +463,7 @@ resource "azurerm_linux_web_app" "beaver-app" {
   app_settings = {
     "AZURE_EVENT_HUB_CONNECTION_STRING" = azurerm_eventhub_namespace.beaver.default_primary_connection_string
     "AZURE_EVENT_HUB_NAME"              = azurerm_eventhub_namespace.beaver.name
-    "WEBHOOK_SECRET"                    = "${data.github_actions_secrets.beaver_secrets}"
+    "WEBHOOK_SECRET"                    = "${var.my_encrypted_secret_value}"
     "APP_ID"                            = "test1"
     "PRIVATE_KEY"                       = "test1"
   }
